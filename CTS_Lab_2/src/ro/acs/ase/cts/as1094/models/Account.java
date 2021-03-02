dd0dd27c57d7751704c543a50563787c4410b255
Mihai Sumanariu <mihai.sumanariu98@gmail.com>
2021-03-02 15:26:06 +0200

    Lab 2 Commit -  second try

--- CTS_Lab_2/src/ro/acs/ase/cts/as1094/models/Account.java
+++ CTS_Lab_2/src/ro/acs/ase/cts/as1094/models/Account.java
@@ -1,9 +1,12 @@
 package ro.acs.ase.cts.as1094.models;
 
+import ro.acs.ase.cts.as1094.exceptions.IllegalTransferException;
+import ro.acs.ase.cts.as1094.exceptions.InsufficientFundsException;
+
 public abstract class Account {
 	
 	public abstract void deposit(Double amount);
-	public abstract void withdraw(Double amount);
+	public abstract void withdraw(Double amount) throws InsufficientFundsException;
 	public abstract Object getBalance();
-	public abstract void transfer(Account destination, Double amount);
+	public abstract void transfer(Account destination, Double amount) throws InsufficientFundsException, IllegalTransferException;
 }
