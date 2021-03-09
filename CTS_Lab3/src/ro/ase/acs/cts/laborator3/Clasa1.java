870922e9e94c44bec3b61605360a65f2c10803f2
Mihai Sumanariu <mihai.sumanariu98@gmail.com>
2021-03-09 14:46:10 +0200

    Before break

--- CTS_Lab3/src/ro/ase/acs/cts/laborator3/Clasa1.java
+++ CTS_Lab3/src/ro/ase/acs/cts/laborator3/Clasa1.java
@@ -1,26 +1,56 @@
 package ro.ase.acs.cts.laborator3;
 
 public class Clasa1 {
-	public float Calculeaza(int t, float s, int perioada)
+	
+	public static final int MAX_AGE_ACCOUNT=10;
+	public static final float MAX_FIDELITY_DISCOUNT=0.15f;
+	
+	public float computePriceWithDiscount(ProductType productType, float price, int accountAge)
 	  {
-	    float rezultat = 0;
-	    float valoare = (perioada > 10) ? (float)15/100 : (float)perioada/100; 
-	    if (t == 1)
+	    float finalPrice = 0;
+	    float fidelityDiscount = (accountAge > MAX_AGE_ACCOUNT) ? MAX_FIDELITY_DISCOUNT : (float)accountAge/100; 
+	    if (productType == ProductType.NEW)
 	    {
-	      rezultat = s;
+	      finalPrice = price;
 	    }
-	    else if (t == 2)
+	    else if (productType == ProductType.DISCOUNTED)
 	    {
-	      rezultat = (s - (0.1f * s)) - valoare * (s - (0.1f * s));
+	      finalPrice = (price - (ProductType.DISCOUNTED.getDiscountValue() * price)) - fidelityDiscount * (price - (ProductType.DISCOUNTED.getDiscountValue() * price));
 	    }
-	    else if (t == 3)
+	    else if (productType == ProductType.LIMITED_STOCK)
 	    {
-	      rezultat = (s - (0.25f * s)) - valoare * (s - (0.25f * s));
+	      finalPrice = (price - (ProductType.LIMITED_STOCK.getDiscountValue() * price)) - fidelityDiscount * (price - (ProductType.LIMITED_STOCK.getDiscountValue() * price));
 	    }
-	    else if (t == 4)
+	    else if (productType == ProductType.LEGACY)
 	    {
-	      rezultat = (s - (0.35f * s)) - valoare * (s - (0.35f * s));
+	      finalPrice = (price - (ProductType.LEGACY.getDiscountValue() * price)) - fidelityDiscount * (price - (ProductType.LEGACY.getDiscountValue() * price));
 	    }
-	    return rezultat;
+	    return finalPrice;
 	  }
 }
+
+
+/*public class Clasa1 {
+public float Calculeaza(int t, float s, int perioada)
+{
+  float rezultat = 0;
+  float valoare = (perioada > 10) ? (float)15/100 : (float)perioada/100; 
+  if (t == 1)
+  {
+    rezultat = s;
+  }
+  else if (t == 2)
+  {
+    rezultat = (s - (0.1f * s)) - valoare * (s - (0.1f * s));
+  }
+  else if (t == 3)
+  {
+    rezultat = (s - (0.25f * s)) - valoare * (s - (0.25f * s));
+  }
+  else if (t == 4)
+  {
+    rezultat = (s - (0.35f * s)) - valoare * (s - (0.35f * s));
+  }
+  return rezultat;
+}
+}*/
