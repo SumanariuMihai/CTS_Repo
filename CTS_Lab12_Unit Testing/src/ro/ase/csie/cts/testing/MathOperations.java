65ab32fea19f536f6920ce07ba9de383f49a783d
Mihai Sumanariu <mihai.sumanariu98@gmail.com>
2021-05-18 15:32:10 +0300

    Week 12 - Unit Testing - before seminar end

---
+++ CTS_Lab12_Unit Testing/src/ro/ase/csie/cts/testing/MathOperations.java
@@ -0,0 +1,14 @@
+package ro.ase.csie.cts.testing;
+
+import ro.ase.csie.cts.testing.exceptions.DivisionByZeroException;
+
+public class MathOperations {
+	
+	public static int add(int a, int b) { return a+b;}
+	
+	public static double divide (int a, int b) throws DivisionByZeroException {
+		if(b==0)
+			throw new DivisionByZeroException();
+		return a/b;
+		}
+}
---
+++ CTS_Lab12_Unit Testing/src/ro/ase/csie/cts/testing/main/App.java
@@ -0,0 +1,24 @@
+package ro.ase.csie.cts.testing.main;
+
+import ro.ase.csie.cts.testing.MathOperations;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println("App started!");
+
+		//"This is for testing - Uncomment if u need"
+		/*
+		int a=5;
+		int b=6;
+		int expectedResult=11;
+		int actualResult=MathOperations.add(a, b);
+		if(actualResult==expectedResult)
+			System.out.println("I know how to add");
+		else
+			System.out.println("I am dumb, let's get some math classes");
+			*/
+	}
+
+}
